36c38c6e23b878519cf3d41f5a020363defce715
svidhaji <svidhaji@gmail.com>
2019-09-30 11:34:19 +0300

    hello text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
     </View>
   );
 };
