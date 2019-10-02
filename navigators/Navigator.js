72389dd92fa38ce7e154878e9033fa3b456d6ae2
KulaginArtur <artur.kulagin@gmail.com>
2019-10-02 11:36:10 +0300

    Added header and icons in TabNavigator

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/display-name */
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createStackNavigator} from 'react-navigation-stack';
@@ -6,24 +8,31 @@ import Profile from '../views/Profile';
 import Single from '../views/Single';
 import AuthLoading from '../views/AuthLoading';
 import Login from '../views/Login';
+import {Icon} from 'native-base';
 
 const TabNavigator = createBottomTabNavigator(
     {
-      Home: {
-        screen: Home,
-        navigationOptions: {
-          title: 'Home',
-        },
-      },
-      Profile: {
-        screen: Profile,
-        navigationOptions: {
-          title: 'Profile',
-        },
-      },
+      Home,
+      Profile,
     },
     {
-      initialRouteName: 'Home',
+      defaultNavigationOptions: ({navigation}) => ({
+        tabBarIcon: () => {
+          const {routeName} = navigation.state;
+          let iconName;
+          if (routeName === 'Home') {
+            iconName = 'home';
+          } else if (routeName === 'Profile') {
+            iconName = 'person';
+          }
+
+          // You can return any component that you like here!
+          return <Icon
+            name={iconName}
+            size={25}
+          />;
+        },
+      }),
     }
 );
 
