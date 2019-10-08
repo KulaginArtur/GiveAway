4da3a05a3146c7adf137d57346eeea529deee039
svidhaji <svidhaji@gmail.com>
2019-10-08 15:36:44 +0300

    Add Tag done, requesting specific tag in progress..

--- hooks/UploadHooks.js
+++ hooks/UploadHooks.js
@@ -1,12 +1,13 @@
 import {useState, useContext} from 'react';
 import mediaAPI from './ApiHooks';
+import {AsyncStorage} from 'react-native';
 import {MediaContext} from '../contexts/MediaContext';
 
 const {uploadFile, reloadAllMedia} = mediaAPI();
 
 const useUploadForm = () => {
   const initInputs = {title: '', description: ''};
-  const [inputs, setInputs] = useState(initInputs);
+  const [inputs, setInputs] = useState({});
   const {setMedia, setMyMedia} = useContext(MediaContext);
   // upload form event handlers
   const handleTitleChange = (text) => {
@@ -24,45 +25,55 @@ const useUploadForm = () => {
     console.log('insert desc: ', text);
   };
 
-  const moreData = {
-    description: inputs.description,
-    app: 'GiveAway',
-  };
-
-  const handleUpload = (file, setLoading, navigation) => {
+  const handleUpload = async (file, setLoading, navigation) => {
+    const userToken = await AsyncStorage.getItem('userToken');
     const fd = new FormData();
     const filename = file.uri.split('/').pop();
-
-    // Infer the type of the image
     const match = /\.(\w+)$/.exec(filename);
     let type = '';
     if (file.type === 'image') {
       type = match ? `image/${match[1]}` : `image`;
-      // fix jpg mimetype
-      if (type === 'image/jpg') {
-        type = 'image/jpeg';
-      }
-    } else {
-      type = match ? `video/${match[1]}` : `video`;
     }
-
-    // Upload the image using the fetch and FormData APIs
-    // Assume "photo" is the name of the form field the server expects
+    if (type === 'image/jpg') {
+      type = 'image/jpeg';
+    }
     fd.append('file', {uri: file.uri, name: filename, type});
     fd.append('title', inputs.title);
-    fd.append('description', JSON.stringify(moreData));
+    fd.append('description', inputs.description);
+    // fd.append('tag', JSON.stringify(tagData));
+
     uploadFile(fd).then((response) => {
       console.log('upl resp', response);
+      addTag(response.file_id, userToken).then(()=> {
+        setMedia([]);
+        setTimeout(() => {
+          reloadAllMedia(setMedia, setMyMedia);
+          setLoading(false);
+          navigation.navigate('Home');
+        }, 2000);
+      });
       // reset media because silly refresh problems
-      setMedia([]);
-      setTimeout(() => {
-        reloadAllMedia(setMedia, setMyMedia);
-        setLoading(false);
-        navigation.navigate('Home');
-      }, 2000);
     }).catch((err) => {
       console.log(err);
     });
+  };
+
+  const addTag = async (id, userToken) => {
+    const tagData = {
+      file_id: id,
+      tag: 'GiveAway',
+    };
+
+    const response = await fetch('http://media.mw.metropolia.fi/wbma/tags', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'x-access-token': userToken,
+      },
+      body: JSON.stringify(tagData),
+    });
+    const json = await response.json();
+    console.log('tag added', json);
   };
 
   const resetForm = (setFile) => {
@@ -78,5 +89,4 @@ const useUploadForm = () => {
     resetForm,
   };
 };
-
 export default useUploadForm;
