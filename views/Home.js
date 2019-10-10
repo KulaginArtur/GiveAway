a05a0219f25a1d0a526417a3d6108b72f0e75115
svidhaji <svidhaji@gmail.com>
2019-10-10 16:36:27 +0300

    Statusbar hidden HomePage

--- views/Home.js
+++ views/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import List from '../components/List';
 import PropTypes from 'prop-types';
 import mediaAPI from '../hooks/ApiHooks';
+import {StatusBar} from 'react-native';
 import {Header, Container, Content, Left, Button, Icon, Body, Title, Right} from 'native-base';
 
 
@@ -16,6 +17,7 @@ const Home = (props) => {
   return (
     <Container>
       <Header>
+        <StatusBar hidden={true}/>
         <Left>
           <Button transparent onPress={() =>{
             navigation.push('Upload', {file: singleMedia});
